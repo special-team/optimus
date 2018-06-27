76f71801e5b2e910097a04b7e2d165ddb5743306
ooknight <ooknight@163.com>
2018-06-27 10:45:08 +0800

    update

--- optimus-support-plugins/optimus-core-web-security/src/main/java/com/github/ooknight/rubik/optimus/client/web/SecurityAutoConfiguration.java
+++ optimus-support-plugins/optimus-core-web-security/src/main/java/com/github/ooknight/rubik/optimus/client/web/SecurityAutoConfiguration.java
@@ -1,45 +1,13 @@
 package com.github.ooknight.rubik.optimus.client.web;
 
-import com.github.ooknight.rubik.core.client.web.security.authentication.StandardSessionUserService;
-import com.github.ooknight.rubik.optimus.archer.platform.service.SecurityUserService;
-import com.github.ooknight.rubik.optimus.client.web.trigger.AuthenticationSuccessTrigger;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import javax.annotation.Resource;
 
 @EnableWebSecurity
 public class SecurityAutoConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Resource
-    private SecurityUserService securityUserService;
-
-    @Bean
-    protected AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider r = new DaoAuthenticationProvider();
-        r.setHideUserNotFoundExceptions(false);
-        r.setUserDetailsService(userDetailsService());
-        return r;
-    }
-
-    @Bean
-    protected AuthenticationSuccessTrigger authenticationSuccessTrigger() {
-        return new AuthenticationSuccessTrigger(securityUserService);
-    }
-
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return new StandardSessionUserService(securityUserService);
-    }
 
     @Override
     public void configure(WebSecurity web) {
