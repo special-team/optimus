b8f6e6d048095c0cc98f8f00d7834074e13b776a
ooknight <ooknight@163.com>
2019-03-06 02:16:27 +0800

    update

--- optimus-support-plugins/optimus-core-domain/src/main/java/com/github/ooknight/rubik/core/entity/Active.java
+++ optimus-support-plugins/optimus-core-domain/src/main/java/com/github/ooknight/rubik/core/entity/Active.java
@@ -1,10 +1,7 @@
 package com.github.ooknight.rubik.core.entity;
 
-import io.ebean.annotation.EnumValue;
-
 public enum Active implements State {
-    //
-    @EnumValue("1") ENABLED,
-    @EnumValue("0") LOCKED,
-    @EnumValue("-1") DISABLED
+    ENABLED,
+    LOCKED,
+    DISABLED
 }
